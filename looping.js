8931f8dc61c4359385ff7dd391fe0841eb6524fc
jaiakash95 <jaiakashv.22aim@kongu.edu>
2024-12-28 11:22:47 +0530

    day2 looping and objtype is added

---
+++ looping.js
@@ -0,0 +1,25 @@
+array = [10, 20, 30, 40, 50, "KEC", true]
+//console.log(array);
+
+// for(var a in array)
+// {
+//     console.log(a);
+// }
+
+// for(var b in array)
+// {
+//     console.log(array[b]);
+// }
+
+var var1 = 10
+var var2 = 20
+var sum = var1+var2
+console.log(sum)
+
+var rel1 = 25
+var rel2 = "25"
+if (rel1 == rel2)
+{
+    console.log("Both the values are not same so it's printing true.");
+    console.log(rel1 === rel2);
+}
---
+++ objtype.js
@@ -0,0 +1,6 @@
+objtype=[]
+objtype["movie"]="Enthiran"
+objtype["budget"]="200 cr"
+objtype["boxoffice"]="850 cr"
+objtype["release"]="WorldWide"
+console.log(objtype["boxoffice"]);
