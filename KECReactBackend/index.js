45619b953bd400fc8f9444dd631b8c3afe77d9a5
jaiakash95 <jaiakashv.22aim@kongu.edu>
2025-01-03 14:22:03 +0530

    Closing CRUD operations

--- KECReactBackend/index.js
+++ KECReactBackend/index.js
@@ -1,16 +1,61 @@
-var express = require('express')
-var app = express()
-const PORT = 3001
-app.get('/',(req, res) =>{
-    res.send("Welcome to Backend Server")
+var express = require("express");
+var path = require("path");
+var mdb = require("mongoose");
+var User = require('./models/users')
+var app = express();
+const PORT = 3001;
+app.use(express.json())
+
+mdb
+  .connect("mongodb://localhost:27017") //127.0.0.1:27017 if connection fails
+  .then(() => {
+    console.log("MongoDB Connection Successful");
+  })
+  .catch(() => {
+    console.log("Check your connection String");
+  });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Backend Server");
+});
+app.get("/json", (req, res) => {
+  res.json({ server: "Welcome to Backend", url: "localhost", port: PORT });
+});
+app.get("/static", (req, res) => {
+  //console.log(__dirname);
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post('/signup', (req, res)=>{
+    console.log(req.body)
+    var {firstName, lastName, email} = req.body;
+    //console.log(firstName, lastName, email);
+    try{
+        var newUser = new User({
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        })
+        newUser.save()
+        console.log("User Added Successfully")
+        res.status(200).send("User Added Successfully")
+    }
+    catch(err){
+        console.log(err);
+        res.send(err) 
+    }
+    //res.send("Hello")
 })
-app.get('/json',(req, res) =>{
-    res.json({server:"Welcome to Backend",url:"localhost",port:PORT})
-})
-app.get('/static',(req, res) =>{
-    res.sendFile('D:/FullStack/KECReactBackend/index.html')
+app.get('/getsignup',async(req, res) =>{
+  try{
+    var allSignUpRecords = await User.find()
+    res.json(allSignUpRecords)
+    console.log("All Data Fetched from Database")
+  }
+  catch(err){
+    console.log("Cannot able to read the Records")
+  }
 })
 
-app.listen(PORT,() =>{
-    console.log(`Backend Server Started\nURL: http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Backend Server Started\nURL: http://localhost:${PORT}`);
+});
